ab37b6901cd99b6ec2de6d9ab189c43aef483ccd
Ryusei Hata <hatar@murase.m.is.nagoya-u.ac.jp>
2021-03-10 21:09:15 +0900

    cbowの作成1

---
+++ cbow/model.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        # self.x = x
+        return out
